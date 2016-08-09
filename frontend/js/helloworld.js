04f2d0b08e3b0a276ecc8b910e05df74b97e9a44
Jamie <janderson@icstars.org>
2016-08-09 18:28:45 +0000

    Jamie | Added playbutton and signup buttons on landing page

--- frontend/js/helloworld.js
+++ frontend/js/helloworld.js
@@ -1,9 +1,7 @@
 'use strict'
 
 $(document).ready(function(){
-	$('#hiddenMessageBtn').click(function(val){
-		var name = $('#name').val();
-		$('#hiddenMessage').show();
-		$('#insertName').html(name);
+	$('#menuoptions').click(function(val){
+	
 	});
 });
