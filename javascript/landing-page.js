f9860ff3e6b02ef47c279dafbb54d1863083ddcb
Jamie <janderson@icstars.org>
2016-08-16 13:04:07 +0000

    Jamie|Animations and styles added to landing page

--- javascript/landing-page.js
+++ javascript/landing-page.js
@@ -1,21 +1,100 @@
 
 $(document).ready(function(){
     
-    
+        $(".intro").hide();
         $(".name").hide();
         $(".gobutton").hide();
         
-    
-    $(".BeanBean").click(function(){
-        $(".name").show();
-        $(".gobutton").show();
+    //BeanBean growing and shrinking animation
+    var $img = $('.BeanBean'),
+        scale = 1.1,
+        h = $img.height(),
+        sh = h*scale;
+
+    function scaleUp($elt){
+        $elt.animate({height: sh}, function (){
+            scaleDown($elt);
+        });
+    }
+
+    function scaleDown($elt){
+        $elt.animate({height: h}, function (){
+            scaleUp($elt);
+        });
+    }
+        
+    //Click avatar for intro bubble
+    $(".BeanBean").one("click", function(){
+        $(".intro").show();
+        $(".BeanBean").stop(); //stop animation on click
     });
     
-    /*function enteryourname() {
-        if()
-     }
+    //intro growing and shrinking animation
+    /*var $p = $('.intro'),
+        scale = 1.1,
+        h = $p.height(),
+        sh = h*scale;
+
+    function scaleUp($elt){
+        $elt.animate({height: sh}, function (){
+            scaleDown($elt);
+        });
+    }
+
+    function scaleDown($elt){
+        $elt.animate({height: h}, function (){
+            scaleUp($elt);
+        });
     }*/
+    
+    $(".intro").click(function(){
+        $(".name").show();
+        $(".gobutton").show();
+        $(".intro").hide();
+    });
+    
+    //will only allow lower-case letters a-z and numbers 1-9
+    $(".name").bind('keyup blur',function(){ 
+    var node = $(this);
+    node.val(node.val().replace(/[^a-z, 1-9]/g,'') ); }
+    );
+   
+
+    scaleUp($img); 
+    
+    //star animation
+    var limit=35, // Max number of stars
+    body=document.body;
+        loop={
+         //initilizeing
+        start:function(){
+        for (var i=0; i <= limit; i++) {
+        var star=this.newStar();
+        star.style.top=this.rand()*100+"%";
+        star.style.left=this.rand()*100+"%";
+        star.style.webkitAnimationDelay=this.rand()+"s";
+        star.style.mozAnimationDelay=this.rand()+"s";
+        body.appendChild(star);
+  };
+ },
+    //to get random number
+        rand:function(){
+            return Math.random();
+ },
+ 
+    //creating html dom for star
+        newStar:function(){
+        var d = document.createElement('div');
+        d.innerHTML = '<figure class="star"><figure class="star-top"></figure><figure class="star-bottom"></figure></figure>';
+        return d.firstChild;
+ },
+};
+    loop.start();
+    //end of star animation
 });
+
+
+
 
 
 
--- javascript/level1.js
+++ javascript/level1.js
@@ -1,5 +1,58 @@
 $(document).ready(function(){
   
   
-  
+  $("button").click(function(){
+    $("#avatar").animate({left: '250px'});
+  }
 })
+
+
+
+/*var gamecanvas = $('#gamecanvas'),
+    avatar = $('#avatar'),
+    w = gamecanvas.width() - avatar.width(),
+    d = {},
+    x = 3;
+
+function newv(v,a,b) {
+    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+    return n < 0 ? 0 : n > w ? w : n;
+}
+
+$(window).keydown(function(e) { d[e.which] = true; });
+$(window).keyup(function(e) { d[e.which] = false; });
+
+setInterval(function() {
+    avatar.css({
+        left: function(i,v) { return newv(v, 37, 39); },
+        top: function(i,v) { return newv(v, 38, 40); }
+    });
+}, 20);*/
+
+
+
+/*$(document).keydown(function(e) {
+    switch (e.which) {
+    case 37:
+        $('#avatar').stop().animate({
+            left: '-=10'
+        }); //left arrow key
+        break;
+    case 38:
+        $(#avatar').stop().animate({
+            top: '-=10'
+        }); //up arrow key
+        break;
+    case 39:
+        $('#avatar').stop().animate({
+            left: '+=10'
+        }); //right arrow key
+        break;
+    case 40:
+        $('#avatar').stop().animate({
+            top: '+=10'
+        }); //bottom arrow key
+        break;
+    }
+})*/
+  
