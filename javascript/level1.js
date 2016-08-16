7534f2edb749bb2fff3e3dcd7200d75b43667491
Jamie <janderson@icstars.org>
2016-08-16 19:15:45 +0000

    Jamie|Linked game in progress to planet 2

--- javascript/level1.js
+++ javascript/level1.js
@@ -1,10 +1,44 @@
-$(document).ready(function(){
-  
-  
-  $("button").click(function(){
-    $("#avatar").animate({left: '250px'});
+var theThing = document.querySelector("#thing");
+var container = document.querySelector("#contentContainer");
+ 
+container.addEventListener("click", getClickPosition, false);
+ 
+function getClickPosition(e) {
+    var parentPosition = getPosition(e.currentTarget);
+    var xPosition = e.clientX - parentPosition.x - (theThing.clientWidth / 2);
+    var yPosition = e.clientY - parentPosition.y - (theThing.clientHeight / 2);
+     
+    theThing.style.left = xPosition + "px";
+    theThing.style.top = yPosition + "px";
+}
+ 
+// Helper function to get an element's exact position
+function getPosition(el) {
+  var xPos = 0;
+  var yPos = 0;
+ 
+  while (el) {
+    if (el.tagName == "BODY") {
+      // deal with browser quirks with body/window/document and page scroll
+      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+      var yScroll = el.scrollTop || document.documentElement.scrollTop;
+ 
+      xPos += (el.offsetLeft - xScroll + el.clientLeft);
+      yPos += (el.offsetTop - yScroll + el.clientTop);
+    } else {
+      // for all other non-BODY elements
+      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+    }
+ 
+    el = el.offsetParent;
   }
-})
+  return {
+    x: xPos,
+    y: yPos
+  };
+}
+
 
 
 
@@ -54,5 +88,34 @@ setInterval(function() {
         }); //bottom arrow key
         break;
     }
-})*/
+})
+
+//click the avatar and it runs from the mouse
+var counter = 0;
+    
+    $('#avatar').click( function () {
+        if ( ++counter < 5 ) {
+            var pos =  makeNewPosition();
+            this.style.left = pos[1] +'px';
+            this.style.top  = pos[0] +'px';
+        }
+        else if ( counter = 5 ) {
+            this.style.display = 'none';
+            alert( 'Your message' );
+        }
+    });
+});
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}*/
   
