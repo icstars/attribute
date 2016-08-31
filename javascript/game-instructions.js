34bbdcedead79f98a1f0c2c95bc2d75b77a80935
Jamie <janderson@icstars.org>
2016-08-31 16:27:00 +0000

    Jamie| still figuring login for .type for game-instructions.js

--- javascript/game-instructions.js
+++ javascript/game-instructions.js
@@ -1,18 +1,29 @@
  $(document).ready(function(){
  
-        $("#lineOne").hide();
-        $("#lineTwo").hide();
-        $("#lineThere").hide();
-        $("#lineFour").hide();
-        $("#lineFive").hide();
-        $("#lineSix").hide();
+        // $("#lineOne").hide();
+        // $("#lineTwo").hide();
+        // $("#lineThere").hide();
+        // $("#lineFour").hide();
+        // $("#lineFive").hide();
+        // $("#lineSix").hide();
         
-$(function(){
-      // $("#lineOne").show();
-      $("#lineOne").typed({
-        strings: ["First sentence.", "Second sentence."],
-        typeSpeed: 0
-      });
-  });
-  
+    var str = "<p>This is my <span style='color:red;'>special string</span> with an <img src='http://placehold.it/150x150'> image !</p>",
+        i = 0,
+        isTag,
+        text;
+    
+    (function type() {
+        text = str.slice(0, ++i);
+        if (text === str) return;
+        document.getElementById('typeWriter').innerHTML = text;
+    
+        var char = text.slice(-1);
+        if( char === '<' ) isTag = true;
+        if( char === '>' ) isTag = false;
+    
+        if (isTag) return type();
+        setTimeout(type, 80);
+    }
+    ());
+      
  });
